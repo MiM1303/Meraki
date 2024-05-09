b22f4a08936c21403c600e9e45edf6b17e6894b8
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-05-10 03:20:59 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        briem: `"Briem Hand", cursive`,
+        indie: `"Indie Flower", cursive`,
+        shadows: `"Shadows Into Light", cursive`,
+        playpen: `"Playpen Sans", cursive`,
+        teachers: `"Teachers", sans-serif`
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
