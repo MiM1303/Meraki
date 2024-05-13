4908647fa005d826cde9f66d3af704793b745608
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-05-13 21:59:44 +0600

    added modal for food request

--- src/pages/FoodDetails/FoodDetails.jsx
+++ src/pages/FoodDetails/FoodDetails.jsx
@@ -27,7 +27,86 @@ const FoodDetails = () => {
             <p className="mb-5 text-lg md:text-xl lg:text-2xl"> <span className="font-semibold">Expires By:</span> {date} </p>
             <button onClick={()=>document.getElementById('my_modal_1').showModal()} className="btn lg:mb-6 bg-[#cb946a] md:text-lg lg:text-lg button-styles border-[#60564C]">Request</button>
 
-            
+            {/* REQUEST MODAL */}
+            <dialog id="my_modal_1" className="modal ">
+                <div className="modal-box">
+                    <h3 className="font-bold lg:mt-10 lg:text-3xl text-[#9c6637]">Request for: {name}</h3>
+                    <p className="py-4 text-[#9c6637] ">Press the request button to request the food</p>
+
+            {/* FOOD INFORMATION */}
+            <form className="font-teachers text-[#9c6637] mx-auto grid grid-cols-1 p-2 md:p-8 md:gap-4 text-2xl">
+                    <div className="w-full">
+                        <label className="input input-bordered flex items-center text-base md:text-xl h-16 gap-2">
+                            Photo URL:
+                            <input type="url" className="grow p-1" placeholder="URL for the image of the food" defaultValue={photo} disabled/>
+                        </label>
+                    </div>                
+                      {/* item 1 */}
+                    <div className="w-full">
+                        <label className="input input-bordered flex items-center text-base md:text-xl h-16 gap-2">
+                            Food Name:
+                            <input type="text" className="grow p-1" placeholder="Name of Food" defaultValue={name} disabled/>
+                        </label>
+                    </div>
+                    {/* item 2 */}
+                    <div className="w-full">
+                        <label className="input input-bordered flex items-center text-base md:text-xl h-16 gap-2">
+                            Quantity:
+                            <input type="number" className="grow p-1" placeholder="Quantity of food" defaultValue={quantity} disabled/>
+                        </label>
+                    </div>
+                
+                {/* row-2 */}
+                
+                      {/* item 1 */}
+                    <div className="w-full">
+                        <label className="input input-bordered flex items-center text-base md:text-xl h-16 gap-2">
+                            Location:
+                            <input type="text" className="grow p-1" placeholder="Pickup Location" defaultValue={location} disabled/>
+                        </label>
+                    </div>
+                    {/* item 2 */}
+                    <div className="w-full">
+                        <label className="input input-bordered flex items-center text-base md:text-xl h-16 gap-2">
+                            Expiry Date:
+                            <input type="date" className="grow p-1" placeholder="Expiry date of the food" defaultValue={date} disabled/>
+                        </label>
+                    </div>
+                {/* Additional Notes */}
+                <div className="">
+                    <label className="input input-bordered flex items-center text-base md:text-xl h-16 gap-2">
+                        Additional Notes:
+                        <input type="text" className="grow p-1" defaultValue={notes} placeholder="Additional notes about the food?" disabled/>
+                    </label>
+                </div>
+                <div className="flex flex-col gap-2 text-base md:text-xl">
+                    <h2 className="">Added By:</h2>
+                    <div className="flex flex-col gap-4  items-center mt-4">
+                        <img className="rounded-full" src={userPhoto} alt="" />
+                        <div>
+                            <p>{userName}</p>
+                            <p>{userEmail}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="form-control mt-6">
+                    <button className="btn bg-[#cb946a] button-styles text-base font-bold lg:text-xl  pb-10 pt-4 flex items-center justify-center">
+                        Request
+                    </button>
+                </div>
+            </form>
+                    
+                    
+                    
+                <div className="modal-action ">
+                    <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn">Close</button>
+                    </form>
+                    </div>
+                </div>
+            </dialog>
           </div>
         </div>
       </div>
