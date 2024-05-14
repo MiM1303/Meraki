9bb407bd56824cf6d77798bdbf2bd399bca3bfcc
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-05-15 02:06:06 +0600

    added dynamic layout in featured foods

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,10 +5,16 @@ import FoodCard from "../FoodCard/FoodCard";
 import { Link } from "react-router-dom";
 import Process from "./Process";
 import Reviews from "./Reviews";
+import { BsGridFill } from "react-icons/bs";
+import { BsFillGrid3X2GapFill } from "react-icons/bs";
+import { useState } from "react";
 
 const Home = () => {
     const foods = useLoaderData();
     console.log(foods);
+    const [layout, setLayout] = useState(true);
+    // true -->3 col, false --> 2 col
+    console.log(layout);
     return (
         <div>
             <Helmet>
@@ -18,8 +24,14 @@ const Home = () => {
 
             {/* FEATURED FOODS SECTION */}
             <h2 className="border-b-4 w-fit mx-auto rounded-2xl p-4 border-[#EBB22F] text-center lg:text-4xl text-2xl mt-20 font-semibold text-[#442537]">Featured Foods</h2>
-            <p className="text-center mt-5 lg:text-xl">Have some extra food you would like to give away? Fill out the details below to help someone in need can!</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 mt-16">
+            <p className=" text-center mt-5 lg:text-xl">Have some extra food you would like to give away? Fill out the details below to help someone in need can!</p>
+            <div onClick={()=>setLayout(!layout)} className=" text-[#b6723e] mt-10 shadow-xl w-fit p-2 bg-[#fbece0]">
+                                        {
+                                            layout ? <BsFillGrid3X2GapFill className="text-3xl"/> : <BsGridFill className="text-3xl" />
+                                        }
+                                    </div>
+                                    
+            <div className={layout? 'grid grid-cols-1 md:grid-cols-2 gap-14 mt-16 lg:grid-cols-3 ' : 'grid grid-cols-1 md:grid-cols-2 gap-14 mt-16 lg:grid-cols-2 w-8/12 mx-auto'}>
                 {
                     foods.map(food=><FoodCard 
                         key={food._id}
